d5e081519f172f57c19598d3f6ae3a3cca891c52
david63011 <david630@gmail.com>
2023-01-26 12:45:47 -0300

    add files

--- morse.rb
+++ morse.rb
@@ -1,54 +1,52 @@
 MORSECODE = {
-    ".-" => "a",
-    "-..." => "b",
-    "-.-." => "c",
-    "-.." => "d",
-    "." => "e",
-    "..-." => "f",
-    "--." => "g",
-    "...." => "h",
-    ".." => "i",
-    ".---" => "j",
-    "-.-" => "k",
-    ".-.." => "l",
-    "--" => "m",
-    "-." => "n",
-    "---" => "o",
-    ".--." => "p",
-    "--.-" => "q",
-    ".-." => "r",
-    "..." => "s",
-    "-" => "t",
-    "..-" => "u",
-    "...-" => "v",
-    ".--" => "w",
-    "-..-" => "x",
-    "-.--" => "y",
-    "--.." => "z",
-}
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z'
+}.freeze
 
 def decode_char(char)
-    MORSECODE[char].upcase
+  MORSECODE[char].upcase
 end
 
 def decode_word(word)
-    wordArray = word.split(" ")
-    returnWord = ""
-    for i in 0...wordArray.length
-        returnWord += decode_char(wordArray[i])
-    end
-    returnWord
+  wordArray = word.split
+  returnWord = ''
+  (0...wordArray.length).each do |i|
+    returnWord += decode_char(wordArray[i])
+  end
+  returnWord
 end
 
 def decode(phrase)
-    phraseArray = phrase.split("   ")
-    returnPhrase = ""
+  phraseArray = phrase.split('   ')
+  returnPhrase = ''
 
-    returnPhrase = phraseArray.map{|w| decode_word(w)}.join(" ")
-
-    returnPhrase
+  phraseArray.map { |w| decode_word(w) }.join(' ')
 end
 
-puts decode("-- -.--   -. .- -- .")
+puts decode('-- -.--   -. .- -- .')
 
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
